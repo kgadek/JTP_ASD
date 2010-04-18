e74b90c3d10ee6d1ed40e8b4cbbaa9bb76beae5f
Konrad <kgadek@poczta.fm>
2010-04-18 17:01:53 +0200

    B3: dzialajace dla int,unsigned int

--- b3.c
+++ b3.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
-#define INTprint(_typ) printf("Badany typ: " #_typ \
-	" ______________________________\n"\
-	"\t* rozmiar: %d\n",sizeof(_typ))
-#define BITSEXT(_typ,_n) for(x=1<<(8*sizeof(_typ)-2); x>0; x>>=1)\
-	printf("%d",((x&_n)>0)?1:0); printf("\n");
-#define BITUEXT(_typ,_n) for(x=1<<(8*sizeof(_typ)-1); x>0; x>>=1)\
+#define INTprint(_typ,_n) printf("Badany typ: " #_typ \
+	" ______________________________\n" \
+	"\t* rozmiar:\t\t%8d\n"\
+	"\t* przykladowa wartosc:\t%8d\n"\
+	"\t* binarnie:\t\t[",\
+	sizeof(_typ),_n);\
+	if(_n&1<<(8*sizeof(_typ)-1)) printf("1"); else printf("0"); \
+	for(x=1<<(8*sizeof(_typ)-2); x>0; x>>=1)\
+	printf("%d",((x&_n)>0)?1:0); printf("]\n");
+
+#define BITEXT(_typ,_n) if(_n&1<<(8*sizeof(_typ)-1)) printf("1"); else printf("0"); \
+	for(x=1<<(8*sizeof(_typ)-2); x>0; x>>=1)\
 	printf("%d",((x&_n)>0)?1:0); printf("\n");
 
 int main() {
-    int y=15;
-    unsigned yu=15;
+    unsigned int y=15;
     long x;
-    INTprint(int);
-    BITSEXT(int,y);
+    INTprint(unsigned int,y);
     printf("___\n");
-    BITUEXT(unsigned int,yu)
     return 0;
 }
 
