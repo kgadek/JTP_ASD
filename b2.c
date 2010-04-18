207469e41401f99984e160fb2f985b21253ecea7
Konrad <kgadek@poczta.fm>
2010-04-18 15:11:40 +0200

    B2: +sortowanie

--- b2.c
+++ b2.c
@@ -6,6 +6,7 @@
 
 void fill(double*, int); /* wypelnij tablice losowymi wartosciami
 			    z przedzialu [-10.0, 10.0) */
+void sort(double*, int); /* posortuj tablice algorytmen Insertion Sort*/
 
 int main() {
     double xx[100];
@@ -13,6 +14,7 @@ int main() {
     int i;
 #endif
     fill(xx,100);
+    sort(xx,100);
 #ifdef _DBG
     for(i=0;i<100;++i)
 	printf("%.1f\n",xx[i]);
@@ -21,12 +23,34 @@ int main() {
 }
 
 void fill(double *x, int len) { /*________________________________________*/
- 	/* proces tworzenia przedzialu:
-	   [0,199] -> [-100,99] -> [-10.0, 9.9] <=> [-10.0, 10.0) */
-
+	    /* 
+		x	- wskaznik na tablice
+		len	- rozmiar tablicy
+	       
+		proces tworzenia przedzialu:
+		[0,199] -> [-100,99] -> [-10.0, 9.9] <=> [-10.0, 10.0)
+	    */
     srand((unsigned int)time(NULL)); /* inicjuj generator
 					liczb pseudolosowych */
     while(len--)                     /* wypelnij tablice */
 	x[len] = ((double)(rand()%200-100))/10.0f;
+}
+
+void sort(double *x, int len) { /*________________________________________*/
+	    /*
+		x	- wskaznik na tablice
+		len	- rozmiar tablicy
+	    */
+    int i,j;
+    double mx;
+    for(i=1; i<len; ++i) {
+	mx=x[i];
+	j=i-1;
+	while(j>=0 && x[j]>mx) {
+	    x[j+1] = x[j];
+	    --j;
+	}
+	x[j+1] = mx;
+    }
 }
 
