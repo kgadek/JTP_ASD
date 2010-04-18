cbb59a28d9c3bf880dd1819db4d438e480fbc4c7
Konrad <kgadek@poczta.fm>
2010-04-18 14:16:19 +0200

    B1: dodatkowe komentarze

--- b1.c
+++ b1.c
@@ -2,18 +2,20 @@
 #include <math.h>
 
 
-double pi_a(int);
-double pi_b(int);
+double pi_a(int); /* licz PI ze wzoru Eulera */
+double pi_b(int); /* licz PI ze wzoru Wallisa */
 
 
 int main() {
-    int x;
+    int x; /* x - dokladnosc obliczen (ilosc 
+	      	  wyrazow we wzorze)
+	   	  Zalozenie: 0 <= x <= maxint */
     scanf("%d",&x);
     printf("%1.10f\n%1.10f\n", pi_a(x), pi_b(x));
     return 0;
 }
 
-double pi_a(int n) { /* wzor Eulera na liczbe pi */
+double pi_a(int n) { /* wzor Eulera na liczbe pi ________________________*/
     double ret = 0.0;
     int i = 1;
     for(; i<=n; ++i) {
@@ -22,8 +24,8 @@ double pi_a(int n) { /* wzor Eulera na liczbe pi */
     return sqrt(ret);
 }
 
-double pi_b(int n) { /* wzor Wallisa na liczbe pi */
-    double ret = 2.0;
+double pi_b(int n) { /* wzor Wallisa na liczbe pi _______________________*/
+    double ret = 2.0; /* zaczynam od 2 by nie mnozyc na samym koncu */
     int even = -1,
 	i = 1;
     for(; i<=n; ++i) {
