aa65791ea599b768a84b2f90a490e0a4c3c1aaa4
Konrad <kgadek@poczta.fm>
2010-04-18 18:41:40 +0200

    B3: zrobione?

--- b3.c
+++ b3.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-#define INTTEST(_typ,_fmt,_n,_v) _n=_v; printf("Badany typ: " #_typ \
-	" ____________________________________________________\n" \
+/* Nieco brzydkie makro do obslugi liczb calkowitych.
+
+   Wywolanie:
+   	INTTEST(_typ,_fmt,_n,_v)
+   gdzie:
+   		_typ	- nazwa typu
+		_fmt	- format funkcji printf
+		_n	- nazwa zmiennej tymczasowo przechowujacej wartosc
+		_v	- przykladowa wartosc ktora badamy binarnie
+   Makro wykorzystuje zmienna x, ktora powinna byc dostepna w czasie
+   wywolania makra.
+*/
+#define INTTEST(_typ,_fmt,_n,_v)\
+    	_n=_v;\
+	printf("Badany typ: " #_typ \
+		" _______________________________________________\n" \
 	"\t* rozmiar:\t\t\t%d B\n"\
 	"\t* przykladowa wartosc:\t\t" _fmt "\n"\
 	"\t* binarnie:\t\t\t[",\
@@ -10,12 +24,41 @@
 	for(x=1<<(8*sizeof(_typ)-2); x>0; x>>=1)\
 	printf("%d",((x&_n)>0)?1:0); printf("]\n");
 
-#define BITEXT(_typ,_n) if(_n&1<<(8*sizeof(_typ)-1)) printf("1"); else printf("0"); \
-	for(x=1<<(8*sizeof(_typ)-2); x>0; x>>=1)\
-	printf("%d",((x&_n)>0)?1:0); printf("\n");
+/* Bardzo brzydkie makro do obslugi liczb zmiennoprzecinkowych.
+
+   Wywolanie:
+	FLOATTEST(_typ,_fmt,_n,_v)
+   gdzie:
+		_typ	- nazwa typu
+		_fmt	- format funkcji printf
+		_n	- nazwa zmiennej tymczasowo przechowujacej wartosc.
+			  Z zalozenia zmienna ta jest w unii zp, ktora
+			  jest czyszczona w kazdym ,,wywolaniu" makra, by
+			  uzyskac czysta postac binarna.
+		_v	- przykladowa wartosc
+   Makro wykorzystuje zmienna x, ktora powinna byc dostepna w czasie
+   wywolania makra.
+*/ 
+#define FLOATTEST(_typ,_fmt,_n,_v)\
+    	for(x=0;x<(signed)sizeof(long double);++x) zp.t[x]=0;\
+	_n=_v;\
+	printf("Badany typ: " #_typ \
+		" _______________________________________________\n" \
+	"\t* rozmiar:\t\t\t%d B\n"\
+	"\t* przykladowa wartosc:\t\t" _fmt "\n"\
+	"\t* rozwiniecie binarne:\n\t\t[",\
+	sizeof(_typ),_n);\
+    	for(x=0;x<(signed)sizeof(_typ);++x)\
+	printf("%d%d%d%d%d%d%d%d",(zp.t[x]&1<<7)?1:0,(zp.t[x]&1<<6)?1:0,\
+		(zp.t[x]&1<<5)?1:0,(zp.t[x]&1<<4)?1:0,(zp.t[x]&1<<3)?1:0,\
+		(zp.t[x]&1<<2)?1:0,(zp.t[x]&1<<1)?1:0,(zp.t[x]&1)?1:0);\
+	printf("]\n");
 
 int main() {
+    /* zmienne pomocnicza */
     long x;
+    /* zmienne potrzebne do wyswietlania tymczasowych wartosci
+       jak i ich rozwiniec binarnych */
     short int _short;
     unsigned short int _ushort;
     int _int;
@@ -24,6 +67,13 @@ int main() {
     unsigned long _ulong;
     char _char;
     unsigned char _uchar;
+    union {
+	float _f;
+	double _d;
+	long double _ld;
+	char t[sizeof(long double)];
+    } zp;
+    /* poczatek bloku informacyjnego dla liczb calkowitych i typu char */
     INTTEST(short int,"%d",_short,-11);
     INTTEST(unsigned short int,"%d",_ushort,11);
     INTTEST(int,"%d",_int,-14);
@@ -32,6 +82,10 @@ int main() {
     INTTEST(unsigned long int,"%ld",_ulong,17);
     INTTEST(char,"%d",_char,-21);
     INTTEST(unsigned char,"%d",_uchar,23);
+    /* poczatek bloku informacyjnego dla liczb zmiennoprzecinkowych */
+    FLOATTEST(float,"%f",zp._f,1.123f);
+    FLOATTEST(double,"%f",zp._d,2.246f);
+    FLOATTEST(long double,"%Lf",zp._ld,4.492f);
     return 0;
 }
 
