f2999c22616eb6910efc8b1c72a72e62a2b5be7b
Konrad <kgadek@poczta.fm>
2010-04-18 16:37:41 +0200

    B3: BITSEXT, BITUEXT(nie dziala jeszcze)

--- b3.c
+++ b3.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+#define INTprint(_typ) printf("Badany typ: " #_typ \
+	" ______________________________\n"\
+	"\t* rozmiar: %d\n",sizeof(_typ))
+#define BITSEXT(_typ,_n) for(x=1<<(8*sizeof(_typ)-2); x>0; x>>=1)\
+	printf("%d",((x&_n)>0)?1:0); printf("\n");
+#define BITUEXT(_typ,_n) for(x=1<<(8*sizeof(_typ)-1); x>0; x>>=1)\
+	printf("%d",((x&_n)>0)?1:0); printf("\n");
+
 int main() {
-    printf("Badana typ: %s\n"\
-	    "\t* rozmiar:\t\t\t%8d [bajtow]\n"\
-	    "\t* przykladowa wartosc A:\t%8d\n"\
-	    "\t* przykladowa wartosc B:\t%8d\n",\
-	    "int",sizeof(int), -4, 4);
+    int y=15;
+    unsigned yu=15;
+    long x;
+    INTprint(int);
+    BITSEXT(int,y);
+    printf("___\n");
+    BITUEXT(unsigned int,yu)
     return 0;
 }
 
