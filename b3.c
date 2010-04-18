0beae100fd4c456a4bdc2b3c673f0ed06f3ee352
Konrad <kgadek@poczta.fm>
2010-04-18 17:04:39 +0200

    B3: chout bo Maksowi "trzeba" cos sprawdzic...

--- b3.c
+++ b3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define INTprint(_typ,_n) printf("Badany typ: " #_typ \
+#define INTprint(_typ,_fmt,_n) printf("Badany typ: " #_typ \
 	" ______________________________\n" \
 	"\t* rozmiar:\t\t%8d\n"\
-	"\t* przykladowa wartosc:\t%8d\n"\
+	"\t* przykladowa wartosc:\t" #_fmt "\n"\
 	"\t* binarnie:\t\t[",\
 	sizeof(_typ),_n);\
 	if(_n&1<<(8*sizeof(_typ)-1)) printf("1"); else printf("0"); \
@@ -15,9 +15,9 @@
 	printf("%d",((x&_n)>0)?1:0); printf("\n");
 
 int main() {
-    unsigned int y=15;
+    unsigned long y=15;
     long x;
-    INTprint(unsigned int,y);
+    INTprint(unsigned long,"%lld",y);
     printf("___\n");
     return 0;
 }
