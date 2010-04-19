5b2927877fcaf0755a04daba348d7611125676a8
Konrad <kgadek@poczta.fm>
2010-04-19 15:35:23 +0200

    B2: alokacja pamieci

--- jtp1-2/b2.c
+++ jtp1-2/b2.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 FILE *outf;
+	double *x,*y,*z, **tab;
+
+double odl(int id_1, int id_2) {
+	return sqrt((x[id_1]-x[id_2])*(x[id_1]-x[id_2])+(y[id_1]-y[id_2])*(y[id_1]-y[id_2])+(z[id_1]-z[id_2])*(z[id_1]-z[id_2]));
+}
 
 int main(int argc, char *argv[]) {
-	int i,N;
-	double a,b,c;
+	int i,j,N;
 	if(argc!=2) {
 		fprintf(stderr,"Podano zla ilosc parametrow!\n\n"\
 				\
@@ -25,11 +30,38 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 	fscanf(outf,"%d",&N);
+	x = (double *)malloc((unsigned int)N*sizeof(double));
+	y = (double *)malloc((unsigned int)N*sizeof(double));
+	z = (double *)malloc((unsigned int)N*sizeof(double));
+	tab = (double **)malloc((unsigned int)N*sizeof(double*));
+	if(x==NULL || y==NULL|| z==NULL || tab==NULL) {
+		fprintf(stderr,"Blad alokacji pamieci!\n");
+		if(x)
+			free(x);
+		if(y)
+			free(y);
+		if(z)
+			free(z);
+		if(tab)
+			free(tab);
+		fclose(outf);
+		exit(1);
+	}
 	for(i=0;i<N;++i) {
-		fscanf(outf," (%lf,%lf,%lf) ",&a,&b,&c);
-		/* printf("Wczytalem %f %f %f\n",a,b,c); */
+		(tab[i]) = (double*)calloc((size_t)N,(size_t)sizeof(double));
+		fscanf(outf," (%lf,%lf,%lf) ",x+i,y+i,z+i);
+	}
+	for(i=0;i<N;++i) {
+		for(j=0;j<N;++j) {
+		}
 	}
 	fclose(outf);
+	free(x);
+	free(y);
+	free(z);
+	for(i=0;i<N;++i)
+		free(tab[i]);
+	free(tab);
 	return 0;
 }
 
