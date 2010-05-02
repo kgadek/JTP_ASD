62933b374d031fdc85582ce50f595405bef06125
Konrad Gądek <kgadek@poczta.fm>
2010-05-02 23:36:11 +0200

    B4: rozpoznawanie opcji -f

--- jtp_laborki/jtp1-2/b4.c
+++ jtp_laborki/jtp1-2/b4.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void czytaj(FILE *,int,int*,int*,int*,int*);
 
@@ -6,6 +8,19 @@ int main(int argc, char *argv[]) {
 	FILE *fn;
 	int i, wyr, gcc, gwc, glc, gzc;
 	gcc = gwc = glc = gzc = 0;
+	wyr = -1;
+	for(i=1;i<argc;++i) {
+		if(strcmp("-f",argv[i])==0) {
+			if(i+1<argc) {
+				wyr=argv[i+1][0];
+				/*dbg*/printf("mam -f %c\n",wyr);
+				break;
+			} else {
+				fprintf(stderr,"Nie podano znaku po opcji -f\n");
+				exit(1);
+			}
+		}
+	}
 	/*dbg*/for(i=1;i<argc;++i) printf("%d: %s\n",i,argv[i]);
 	return 0;
 }
