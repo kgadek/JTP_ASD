e298aef24925c9e3d53119111350a28a97c0d5ee
Konrad Gądek <kgadek@poczta.fm>
2010-05-03 01:03:19 +0200

    B4: dodano komentarze; przechodzi testy jak do tej pory

--- jtp_laborki/jtp1-2/b4.c
+++ jtp_laborki/jtp1-2/b4.c
@@ -2,24 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* ==========[ deklaracja funkcji liczacej znaki ]========== */
 void czytaj(FILE *,int,int*,int*,int*,int*);
 
+
+/* ==========[ funkcja main ]========== */
 int main(int argc, char *argv[]) {
-	FILE *fn;
-	int i, j, tmp, wyr, fcnt, maxfn, sh,
-		gcc, gwc, glc, gzc;			/* globalne liczniki znakow, slow, linii i
-									   wyroznionego znaku */
+	FILE *fn;					/* wskaznik na plik */
+	int i, j, tmp,				/* zmienne pomocnicze */
+		wyr,					/* znak, ktorego czestosc wystepowania zliczamy */
+		fcnt,					/* ilosc plikow, ktore odczytano */
+		sh,						/* ilosc plikow podanych na wejsciu */
+		maxfn,					/* dlugosc najdluzszej nazwy pliku */
+		gcc, gwc, glc, gzc;		/* globalne liczniki znakow, slow, linii i
+								   wyroznionego znaku */
+
+
+	/* inicjacja zmiennych */
 	gwc = glc = gzc = fcnt = gcc = 0;
-	maxfn = 5;		/* 5 - dlugosc slowka total */
+	maxfn = 5;	/* 5 - dlugosc slowka total */
 	wyr = -1;
-					/* tu gcc uzywamy jako zmienna pomocnicza */
+
+
+	/* wstepne czytanie opcji - odnalezienie opcji -f, zliczenie plikow (fcnt),
+	   wyznaczenie najdluzszej nazwy pliku (maxfn) */
 	for(i=1;i<argc;++i) {
-		if(strcmp("-f",argv[i])==0) {
-			if(i+1<argc) {
+
+		if(strcmp("-f",argv[i])==0) {	/* jesli znaleziono opcje -f ...*/
+			if(wyr!=-1) {					/* ... ale juz wczesniej wczytana */
+				fprintf(stderr,"Opcje -f podano wielokrotnie\n"\
+						"Wywolanie programu:\n\t"\
+			   			"%s plik1 [plik2] ... [plikM] [-f znak] [plikN] ...\n"\
+						"Opcje:\n\t"\
+						"-f X\t\t- zlicz ilosc wystapien znaku X\n",
+						argv[0]);
+				exit(1);
+			}
+			if(i+1<argc) {					/* ... przetworz argument -f */
 				wyr=argv[i+1][0];
 				++i;
 				continue;
-			} else {
+			} else {						/* ... popros o podanie argumentu -f */
 				fprintf(stderr,"Nie podano znaku po opcji -f\n"\
 						"Wywolanie programu:\n\t"\
 			   			"%s plik1 [plik2] ... [plikM] [-f znak] [plikN] ...\n"\
@@ -29,11 +52,17 @@ int main(int argc, char *argv[]) {
 				exit(1);
 			}
 		}
-		tmp = (int)strlen(argv[i]);
+		/* argument nie jest ani opcja -f ani jej parametrem */
+		tmp = (int)strlen(argv[i]);		/* poprawa wartosci maxfn */
 		if(tmp>maxfn)
 			maxfn = tmp;
-		++fcnt;
+
+		++fcnt;		/* zwiekszenie wartosci fcnt o ile argv[i] to nie -f ani
+					   jej argument */
 	}
+
+
+	/* sprawdzenie, czy podano jakikolwiek plik wejsciowy */
 	if(fcnt<1) {
 		fprintf(stderr,"Nie podano zadnych plikow wejsciowych.\n\n"\
 				"Wywolanie programu:\n\t"\
@@ -43,35 +72,48 @@ int main(int argc, char *argv[]) {
 				argv[0]);
 		exit(1);
 	}
-	sh = fcnt;
-	for(i=1;i<argc;++i) {
-		if(strcmp("-f",argv[i])==0) {
-			++i;		/* pomin opcje -f i jej argument */
+	sh = fcnt;		/* wpisanie do sh ilosci rozpatrywanych plikow wejsciowych */
+
+
+	/* czesc wlasciwa - liczenie znakow */
+	for(i=1;i<argc;++i) {		/* dla kazdego parametru... */
+
+		if(strcmp("-f",argv[i])==0) {		/* jesli parametr to -f ...*/
+			++i;								/* ...to pomin -f i jego argument */
 			continue;
 		}
-		fn = fopen(argv[i],"r");
-		if(fn == NULL ) {
+
+		fn = fopen(argv[i],"r");		/* probuj otworzyc plik */
+
+		if(fn == NULL ) {					/* blad odczytu pliku */
 			fprintf(stderr,"%s - nie mozna odczytac pliku\n",argv[i]);
-			--fcnt;
+			--fcnt;						/* zmniejsz licznik rozpatrzonych plikow */
 			continue;
 		} else {
-			if(sh>1) {
+											/* plik otworzono poprawnie */
+			if(sh>1) {		/* jesli rozpatrujemy wiele plikow, to wyswietl
+							   nazwe aktualnie rozpatrywanego PRZED statystyka
+							   (inaczej niz wc) */
 				printf("%s: ",argv[i]);
-				tmp=maxfn-(int)strlen(argv[i]);
+				tmp=maxfn-(int)strlen(argv[i]);		/* ladne wyrownanie */
 				for(j=0;j<=tmp;++j)
 					printf(" ");
 			}
-			czytaj(fn,wyr,&glc,&gwc,&gcc,&gzc);
-			fclose(fn);
+			czytaj(fn,wyr,&glc,&gwc,&gcc,&gzc);		/* licz znaki */
+			fclose(fn);								/* zamknij plik */
 		}
 	}
+
+	/* podsumowanie statystyk wielu plikow */
 	if(fcnt>1) {		/* ulepszenie w stosunku do wc - wyswietl podsumowanie
-						   tylko jesli ilosc odczytanych plikow byla wieksza
+						   tylko jesli ilosc rozpatrzonych plikow byla wieksza
 						   niz 1 */
 		printf("\ntotal: ");
-		tmp=maxfn-5;
+
+		tmp=maxfn-5;		/* ladne wyrownanie */
 		for(j=0;j<=tmp;++j)
 			printf(" ");
+
 		printf("%5d %5d %5d",glc,gwc,gcc);
 		if(wyr!=-1)
 			printf(" %5d",gzc);
@@ -80,35 +122,50 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+
+
+/* ==========[ definicja funkcji liczacej znaki ]========== */
 void czytaj(FILE *fn, int wyr, int *glc, int *gwc, int *gcc,int *gzc) {
-	int lc,wc,cc,zc,c,word;
+	int lc,wc,cc,zc,	/* liczniki: linii, slow, znakow, znakow X */
+		c,				/* zmienna pomocnicza */
+		word;			/* znacznik rozpatrywania slowa */
+
+	/* inicjacja zmiennych */
 	lc = wc = cc = zc = word = 0;
+
+	/* glowna petla */
 	while( (c = fgetc(fn)) !=  EOF) {
-		++cc;
-		if(c==wyr)
+		++cc;		/* policz znak (tak jak wc) */
+		if(c==wyr)		/* jesli wczytany znak == znakowi X to zwieksz licznik */
 			++zc;
+
 		switch(c) {
-			case 10:		/* \n */
+			case 10:		/* \n - zwieksz licznik nowej linii*/
 				++lc;
-				word = 0;
+				word = 0;		/* konczy slowo */
 				break;
-			case 13:		/* \r 		 uwaga: nie zadziala dobrze na MacOS */
+			case 13:		/* \r  - obsluga windowsowego/macowego CR */
+				word = 0;		/* konczy slowo */
 				break;
 			case 11:		/* tab */
 			case 32:		/* spacja */
-				word = 0;
+				word = 0;		/* oba koncza slowo */
 				break;
 			default:
-				if(!word) {
+				if(!word) {		/* jesli poczatek slowa... */
 					word = 1;
-					++wc;
+					++wc;			/* ...to je zlicz */
 				}
 		}
 	}
+
+	/* wyswietlenie statystyk */
 	printf("%5d %5d %5d",lc,wc,cc);
-	if(wyr!=-1)
+	if(wyr!=-1)		/* jesli trzeba, uwzglednij ilosc wystapien znaku X */
 		printf(" %5d",zc);
 	printf("\n");
+
+	/* popraw liczniki ogolne przekazane przez referencje */
 	*glc += lc;
 	*gwc += wc;
 	*gcc += cc;
