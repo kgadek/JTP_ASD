cd7c2a5630a45d94c9b967789cd0ef9eec6bb3d5
Konrad Gądek <kgadek@poczta.fm>
2010-05-31 20:15:06 +0200

    B4.1 OK:+inline +testy;

--- jtp_laborki/jtp1-4/punkty.cpp
+++ jtp_laborki/jtp1-4/punkty.cpp
@@ -2,67 +2,96 @@
 #include <iostream>
 #include "punkty.h"
 
-/* ====================[ Wektor ]==================== */
+
+//#define NDEBUG	/* do testow klas i metod____________________________________________ */
+#include <cassert>	/* odkomentowac #define NDEBUG by nie testowac */
+#define EQ(a,b) (fabs((double)(a)-(double)(b))<0.0000001)	/* do porownywania double */
+
+
+
+
+/* ========================================[ Wektor ]======================================== */
 Wektor::Wektor() : x(0.0), y(0.0), z(0.0) { }
 
 Wektor::Wektor(const double a,const double b,const double c) : x(a), y(b), z(c) { }
 
-double Wektor::len() const {
+inline double Wektor::len() const {
 	return sqrt(x*x+y*y+z*z);
 }
 
-Wektor Wektor::operator+(const Wektor &w) const {
+inline Wektor Wektor::operator+(const Wektor &w) const {
 	return Wektor(x+w.x, y+w.y, z+w.z);
 }
 
-Wektor& Wektor::operator+=(const Wektor &w) {
+inline Wektor& Wektor::operator+=(const Wektor &w) {
 	x += w.x; y += w.y; z += w.z;
 	return *this;
 }
 
-Wektor Wektor::operator-() const {
+inline Wektor Wektor::operator-() const {
 	return Wektor(-x, -y, -z);
 }
 
-std::ostream& operator<<(std::ostream &out, const Wektor &w) {
+inline std::ostream& operator<<(std::ostream &out, const Wektor &w) {
 	out << "(" << w.x << "," << w.y << "," << w.z << ")";
 	return out;
 }
 
-/* ====================[ Punkt ]===================== */
+/* ========================================[ Punkt ]========================================= */
 Punkt::Punkt() : x(0.0), y(0.0), z(0.0) { }
 
 Punkt::Punkt(const double a,const double b,const double c) : x(a), y(b), z(c) { }
 
-double odl(const Punkt &a, const Punkt &b) {
+inline double odl(const Punkt &a, const Punkt &b) {
 	return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)+(a.z-b.z)*(a.z-b.z));
 }
 
-Punkt operator+(const Punkt &p, const Wektor &w) {
+inline Punkt operator+(const Punkt &p, const Wektor &w) {
 	return Punkt(p.x+w.x, p.y+w.y, p.z+w.z);
 }
 
-Punkt& operator+=(Punkt &p, const Wektor &w) {
+inline Punkt& operator+=(Punkt &p, const Wektor &w) {
 	p.x += w.x; p.y += w.y; p.z += w.z;
 	return p;
 }
 
-Punkt Punkt::operator-() const {
+inline Punkt Punkt::operator-() const {
 	return Punkt(-x,-y,-z);
 }
 
-Punkt Punkt::operator-(const Punkt &p) const {
-	return Punkt(x-p.x, y-p.y, z-p.z);
+inline Wektor Punkt::operator-(const Punkt &p) const {
+	return Wektor(x-p.x, y-p.y, z-p.z);
 }
 
-std::ostream& operator<<(std::ostream &out, const Punkt &w) {
+inline std::ostream& operator<<(std::ostream &out, const Punkt &w) {
 	out << "[" << w.x << "," << w.y << "," << w.z << "]";
 	return out;
 }
 
-/* ====================[  Main  ]==================== */
+/* ========================================[  Main  ]======================================== */
 int main() {
-	std::ios_base::sync_with_stdio(0);
+	std::ios_base::sync_with_stdio(0);			/* CIN/COUT booster */
+
+	/* kilka podstawowych testow poprawnosciowych */
+	assert( EQ(Wektor(3.0,4.0,0.0).len(),5.0) );		/* W.len() */
+	assert( EQ(Wektor(1.0,2.0,3.0).len(),3.74165738677394) );
+	assert( EQ(Wektor().len(),0.0) );
+								/* W+W    W+=W */
+	assert( EQ((Wektor(1.0,2.0,3.0)+Wektor(3.0,2.0,1.0)).len(),6.9282032302755091) );
+	assert( EQ((Wektor()+=Wektor(6.0,6.0,6.0)).len(),10.3923048454132637611) );
+	assert( EQ((-Wektor()).len(),0.0) );			/* -W */
+	assert( EQ((-Wektor(3.0,4.0,5.0)).len(),7.0710678118654) );
+	assert( EQ(((-Wektor(3.1415,2.7182,1.4142))+Wektor(3.1415,2.7182,1.4142)).len(),0.0) );
+	assert( std::cout << Wektor(1.5,2.5,3.5) << '\n' );	/* cout << W */
+	assert( EQ((Punkt()-Punkt(0.0,0.0,0.0)).len(),0.0) );	/* P-P */
+	assert( EQ((Punkt(0.0,10.0,-4.0)-Punkt(-4.0,6.0,-8.0)).len(),6.9282032302755091) );
+								/* -P    P+W    odl(P,P) */
+	assert( EQ(odl(((-Punkt(1.0,2.0,3.0))+Wektor(1.0,2.0,3.0)),Punkt()),0.0) );
+	assert( EQ(odl((Punkt()+Wektor()),Punkt()),0) );	/* P+W    odl(P,P) */
+	assert( EQ(odl(Punkt(),Punkt()),0.0) );			/* odl(P,P) */
+	assert( EQ(odl(Punkt(1.0,2.0,3.0),Punkt(-3.0,-2.0,-1.0)),6.9282032302755091) );
+	assert( std::cout << Punkt(1.23,4.56,7.89) << '\n' );	/* cout << P */
+
 	return 0;
 }
 
--- jtp_laborki/jtp1-4/punkty.h
+++ jtp_laborki/jtp1-4/punkty.h
@@ -2,20 +2,22 @@
 #define _KGADEK_PUNKT_H
 #include <iostream>
 
+/* deklaracje klas */
 class Punkt;
 class Wektor;
 
+/* definicje klas */
 class Wektor {
 	double x,y,z;
 public:
 	Wektor();					/* domyslny konstruktor */
 	Wektor(const double,const double,const double);	/* konstruktor z parametrami*/
-				/* konstruktor kopiujacy automatycznie generowany */
+				/* konstruktor kopiujacy jest automatycznie generowany */
 	double len() const;				/* dlugosc wektora */
 	Wektor operator+(const Wektor&) const;		/* dodawanie wektorow */
 	Wektor& operator+=(const Wektor&);
 	Wektor operator-() const;			/* odwracanie wektora */
-	friend Punkt operator+(const Punkt&,const Wektor&);
+	friend Punkt operator+(const Punkt&,const Wektor&);	/* operacje na W i P */
 	friend Punkt& operator+=(Punkt&, const Wektor&);
 							/* wypisywanie wektora */
 	friend std::ostream& operator<<(std::ostream &, const Wektor &);
@@ -28,9 +30,9 @@ public:
 	Punkt(const double,const double,const double);	/* konstruktor z parametrami */
 	Punkt operator-() const;			/* zwraca punkt symetryczny
 							   wzgledem [0,0,0] */
-	Punkt operator-(const Punkt&) const;		/* odejmowanie punktow */
+	Wektor operator-(const Punkt&) const;		/* odejmowanie punktow */
 	friend double odl(const Punkt&, const Punkt&);	/* odleglosc dwoch punktow */
-	friend Punkt operator+(const Punkt&,const Wektor&);	/* dodawanie punktow */
+	friend Punkt operator+(const Punkt&,const Wektor&);	/* dodawanie P + W */
 	friend Punkt& operator+=(Punkt&, const Wektor&);
 							/* wypisywanie punktu */
 	friend std::ostream& operator<<(std::ostream &, const Punkt &);
