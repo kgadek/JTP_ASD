e4bba99db83904ef329fa18ccf1c5f92dc0d10dc
Konrad G <kgadek@gmail.com>
2010-10-25 02:42:13 +0200

    Laborki z JTP2 z 18.10.2010
    Króciutki programik napisany na zajęciach tworzący tablicę o rozmiarze
    pobranym poprzez argument programu i wypełniający ją kwadratami
    kolejnych liczb naturalnych.

---
+++ jtp2_laborki/jtp2-2/Tablica.java
@@ -0,0 +1,24 @@
+public class Tablica {
+    public static void f(int n) {
+	int [] tab = new int[n+1];
+	for(int i = 0; i<=n; ++i) {
+	    tab[i] = i*i;
+	}
+	for(int j = 0; j<=n; ++j) {
+	    System.out.print("tab[");
+	    System.out.print(j);
+	    System.out.print("] = ");
+	    System.out.println(tab[j]);
+	}
+    }
+    public static void main(String args[]) {
+	try {
+	    if( args.length == 0 )
+		throw new IndexOutOfBoundsException("Nie podano argumentów do programu");
+	    f( Integer.parseInt(args[0]) );
+	} catch( IndexOutOfBoundsException e ) {
+	    System.out.println("ZONK:");
+	    e.printStackTrace();
+	}
+    }
+}
