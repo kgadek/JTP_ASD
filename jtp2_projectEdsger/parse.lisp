52a5d4814df7e8833e11394d5d7076ec167bd784
Konrad G <kgadek@gmail.com>
2011-02-13 21:57:09 +0100

    Przygotowania do przeparsowania plików

--- jtp2_projectEdsger/parse.lisp
+++ jtp2_projectEdsger/parse.lisp
@@ -1,3 +1,5 @@
+(ql:quickload "cl-html-parse")
+
 (defun subtree-search (tree pat)
   (when (and (not (null tree))
 	     (consp tree))
@@ -16,19 +18,63 @@
   (cadar (cddddr x)))
 (defun mpk-read-minutes-sun (x)
   (cadar (cddr (cddddr x))))
-
-(defparameter dt nil)
-
-(ql:quickload "cl-html-parse")
-
-(with-open-file (in "~/mirror/mpk/rozklady.mpk.krakow.pl/aktualne/0144/t001.html")
-  (setf dt
-	(mpk-get-table-from-page
-	 (html-parse:parse-html
-      	  (read-line in nil)))))
-
-;; Pokaż rozkład
-(loop for x in dt do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-norm x)))
-(loop for x in dt do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sat x)))
-(loop for x in dt do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sun x)))
-
+(defun show-tables-raw (filename day)
+  (let ((dt nil))
+    (with-open-file (in filename)
+      (setf dt
+	    (mpk-get-table-from-page
+	     (html-parse:parse-html
+	      (read-line in nil))))
+      (case day
+	((:norm :week) (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
+	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-norm x))))
+	(:sat (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
+	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sat x))))
+	(:sun (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
+	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sun x))))
+	(otherwise nil)))))
+(defun split-string-by-one-space (str)
+  (loop for i = 0 then (1+ j)
+       as j = (position #\Space str :start i)
+       collect (subseq str i j)
+       while j))
+(defun split-string-by-space (str)
+  (delete "" (split-string-by-one-space str) :test #'equal))
+(defun get-ints-from-string (str)
+  (mapcar #'(lambda(x) (parse-integer x :junk-allowed t))
+	  (split-string-by-space str)))
+(defun print-tag (name atts &optional (closingp nil))
+  (format nil "~&<~A~A~{~A~}>~%"
+	  (if closingp "/" "")
+	  (string-downcase name)
+	  (mapcar #'(lambda (att)
+		      (format nil " ~A=\"~A\""
+			      (string-downcase (car att))
+			      (cdr att)))
+		  atts)))
+(defmacro tag (name atts &body body)
+  `(concatenate 'string
+		(print-tag ',name
+			   (list ,@(mapcar #'(lambda(x)
+					       `(cons ',(car x) ,(cadr x)))
+					   atts)))
+		,@body
+		(print-tag ',name nil t)))
+(defun show-tables-xml (filename day)
+  (let ((dt nil))
+    (with-open-file (in filename)
+      (setf dt
+	    (mpk-get-table-from-page
+	     (html-parse:parse-html
+	      (read-line in nil))))
+      (case day
+	((:norm :week) (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
+	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-norm x))))
+	(:sat (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
+	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sat x))))
+	(:sun (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
+	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sun x))))
+	(otherwise nil)))))
+(defun show-tables (line stop day)
+  (show-tables-raw (format nil "rozklady.mpk.krakow.pl/aktualne/~4,'0D/~4,'0Dt~3,'0D.htm.html" line line stop)
+		   day))
