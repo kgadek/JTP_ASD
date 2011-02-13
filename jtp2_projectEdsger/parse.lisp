d1b2acc6c3bb2fc359a57e2e6bc1d0fed7ed93f4
Konrad G <kgadek@gmail.com>
2011-02-13 22:35:09 +0100

    Mini-DSL do generowania XML-a.

--- jtp2_projectEdsger/parse.lisp
+++ jtp2_projectEdsger/parse.lisp
@@ -8,6 +8,7 @@
 	       (list (car tree))
 	       (subtree-search (car tree) pat))
 	   (subtree-search (cdr tree) pat))))
+
 (defun mpk-get-table-from-page (x)
   (cddr (cadadr (caddar (cdaddr (caddar x))))))
 (defun mpk-read-hour (x)
@@ -18,6 +19,7 @@
   (cadar (cddddr x)))
 (defun mpk-read-minutes-sun (x)
   (cadar (cddr (cddddr x))))
+
 (defun show-tables-raw (filename day)
   (let ((dt nil))
     (with-open-file (in filename)
@@ -33,6 +35,7 @@
 	(:sun (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
 	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sun x))))
 	(otherwise nil)))))
+
 (defun split-string-by-one-space (str)
   (loop for i = 0 then (1+ j)
        as j = (position #\Space str :start i)
@@ -43,6 +46,7 @@
 (defun get-ints-from-string (str)
   (mapcar #'(lambda(x) (parse-integer x :junk-allowed t))
 	  (split-string-by-space str)))
+
 (defun print-tag (name atts &optional (closingp nil))
   (format nil "~&<~A~A~{~A~}>~%"
 	  (if closingp "/" "")
@@ -60,13 +64,34 @@
 					   atts)))
 		,@body
 		(print-tag ',name nil t)))
-(defun show-tables-xml (filename day)
+
+;; Line -- numer linii
+(defmacro :l (n &body body)
+  `(tag l ((n ,n)) ,@body))
+;; Day -- dzień (0 - roboczy, 1 - sobota, 2 - niedziela)
+(defmacro :d (d &body body)
+  `(tag d ((d ,d)) ,@body))
+;; Stop -- przystanek
+(let ((stops (make-hash-table))
+      (cnt 0))
+  (defmacro :s (x &body body)
+    (when (not (gethash x stops))
+      (incf cnt)
+      (setf (gethash x stops) cnt))
+    `(tag s ((i ,(gethash x stops))) ,@body)))
+;; Time -- godzina odjazdu
+(defmacro :t (h m)
+  `(tag t nil (tag h nil (format nil "~A" ,h)) (tag m nil (format nil "~A" ,m))))
+
+(defun show-line-xml (filename day)
   (let ((dt nil))
     (with-open-file (in filename)
       (setf dt
 	    (mpk-get-table-from-page
 	     (html-parse:parse-html
 	      (read-line in nil))))
+      (loop for day from 0 to 2 do
+	   
       (case day
 	((:norm :week) (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
 	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-norm x))))
@@ -74,7 +99,7 @@
 	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sat x))))
 	(:sun (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
 	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sun x))))
-	(otherwise nil)))))
+	(otherwise nil))))))
 (defun show-tables (line stop day)
   (show-tables-raw (format nil "rozklady.mpk.krakow.pl/aktualne/~4,'0D/~4,'0Dt~3,'0D.htm.html" line line stop)
 		   day))
