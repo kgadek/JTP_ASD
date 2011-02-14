d3b95976f416a29c8db25418a1be66acad8a6f76
Konrad G <kgadek@gmail.com>
2011-02-14 01:06:45 +0100

    Przepisany DSL dla XML-a.

--- jtp2_projectEdsger/parse.lisp
+++ jtp2_projectEdsger/parse.lisp
@@ -1,14 +1,10 @@
 (ql:quickload "cl-html-parse")
 
-(defun subtree-search (tree pat)
-  (when (and (not (null tree))
-	     (consp tree))
-    (nconc (if (and (consp (car tree))
-		    (eql (caar tree) pat))
-	       (list (car tree))
-	       (subtree-search (car tree) pat))
-	   (subtree-search (cdr tree) pat))))
-
+;;; ################################################################################
+;;; MPK
+;;; ################################################################################
+(defun mpk-get-filename (line stop)
+  (format nil "rozklady.mpk.krakow.pl/aktualne/~4,'0D/~4,'0Dt~3,'0D.htm.html" line line stop))
 (defun mpk-get-table-from-page (x)
   (cddr (cadadr (caddar (cdaddr (caddar x))))))
 (defun mpk-read-hour (x)
@@ -19,7 +15,69 @@
   (cadar (cddddr x)))
 (defun mpk-read-minutes-sun (x)
   (cadar (cddr (cddddr x))))
+(defun mpk-read-stop (x)
+  (cadr (cadadr (caddar (cdadar (cdadar (cdaddr (caddar dt))))))))
 
+
+;;; ################################################################################
+;;; String/Lisp operations
+;;; ################################################################################
+(defun split-string-by-one-space (str)
+  (loop for i = 0 then (1+ j)
+       as j = (position #\Space str :start i)
+       collect (subseq str i j)
+       while j))
+(defun split-string-by-space (str)
+  (delete "" (split-string-by-one-space str) :test #'equal))
+(defmacro split (val yes no)
+  (let ((g (gensym)))
+    `(let ((,g ,val))
+       (if ,g
+	   (let ((head (car ,g))
+		 (tail (cdr ,g)))
+	     ,yes)
+	   ,no))))
+(defun pairs (lst)
+  (labels ((f (lst acc)
+	     (split lst
+		    (if tail
+			(f (cdr tail)
+			   (cons (cons head (car tail))
+				 acc))
+			(reverse acc))
+		    (reverse acc))))
+    (f lst nil)))
+
+
+;;; ################################################################################
+;;; XML
+;;; ################################################################################
+(defun tag1% (name &key attr single closing)
+  (format nil "<~A~A~A~A>~%"
+	  (if closing "/" "")
+	  (string-downcase name)
+	  (if attr
+	      (reduce #'(lambda(a b)
+			  (concatenate 'string a b))
+		      (loop for i in (pairs attr)
+			 collect (format nil " ~a=\"~a\"" (car i) (cdr i))))
+	      "")
+	  (if single " /" "")))
+(defmacro tag1 (name &key attr single closing noenter)
+  `(tag1% ',name :attr ',attr :single ,single :closing ,closing))
+(defmacro tag (name attr &body body)
+  `(concatenate 'string
+		(tag1 ,name :attr ,attr)
+		,@body
+		(tag1 ,name :closing t)))
+(defmacro :xml (&body body)
+  `(concatenate 'string
+		"<?xml version=\"1.0\" encoding=\"UTF-8\"?>~%"
+		,@body))
+
+;;; ################################################################################
+;;; Printing
+;;; ################################################################################
 (defun show-tables-raw (filename day)
   (let ((dt nil))
     (with-open-file (in filename)
@@ -35,71 +93,17 @@
 	(:sun (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
 	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sun x))))
 	(otherwise nil)))))
-
-(defun split-string-by-one-space (str)
-  (loop for i = 0 then (1+ j)
-       as j = (position #\Space str :start i)
-       collect (subseq str i j)
-       while j))
-(defun split-string-by-space (str)
-  (delete "" (split-string-by-one-space str) :test #'equal))
-(defun get-ints-from-string (str)
-  (mapcar #'(lambda(x) (parse-integer x :junk-allowed t))
-	  (split-string-by-space str)))
-
-(defun print-tag (name atts &optional (closingp nil))
-  (format nil "~&<~A~A~{~A~}>~%"
-	  (if closingp "/" "")
-	  (string-downcase name)
-	  (mapcar #'(lambda (att)
-		      (format nil " ~A=\"~A\""
-			      (string-downcase (car att))
-			      (cdr att)))
-		  atts)))
-(defmacro tag (name atts &body body)
-  `(concatenate 'string
-		(print-tag ',name
-			   (list ,@(mapcar #'(lambda(x)
-					       `(cons ',(car x) ,(cadr x)))
-					   atts)))
-		,@body
-		(print-tag ',name nil t)))
-
-;; Line -- numer linii
-(defmacro :l (n &body body)
-  `(tag l ((n ,n)) ,@body))
-;; Day -- dzień (0 - roboczy, 1 - sobota, 2 - niedziela)
-(defmacro :d (d &body body)
-  `(tag d ((d ,d)) ,@body))
-;; Stop -- przystanek
-(let ((stops (make-hash-table))
-      (cnt 0))
-  (defmacro :s (x &body body)
-    (when (not (gethash x stops))
-      (incf cnt)
-      (setf (gethash x stops) cnt))
-    `(tag s ((i ,(gethash x stops))) ,@body)))
-;; Time -- godzina odjazdu
-(defmacro :t (h m)
-  `(tag t nil (tag h nil (format nil "~A" ,h)) (tag m nil (format nil "~A" ,m))))
-
-(defun show-line-xml (filename day)
-  (let ((dt nil))
-    (with-open-file (in filename)
-      (setf dt
-	    (mpk-get-table-from-page
-	     (html-parse:parse-html
-	      (read-line in nil))))
-      (loop for day from 0 to 2 do
-	   
-      (case day
-	((:norm :week) (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
-	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-norm x))))
-	(:sat (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
-	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sat x))))
-	(:sun (loop for x in dt when (not (null (mpk-read-minutes-norm x)))
-	      do (format t "~A:~A~%" (mpk-read-hour x) (mpk-read-minutes-sun x))))
-	(otherwise nil))))))
+(defun show-precise-xml (line stop day)
+  (let ((dt nil)
+	(stop-name "?"))
+    (with-open-file (in (mpk-get-filename line stop))
+      (setf dt (html-parse:parse-html (read-line in nil))))
+    (setf stop-name (mpk-read-stop dt)
+	  dt (mpk-get-table-from-page dt))
+    (format t "Nazwa: ~A~%" stop-name)
+    (:l line (eval `(:s ,stop-name
+			(reduce #'(lambda(a b) (concatenate 'string a b))
+				(loop for d from 0 to 2
+				   collect (:d d))))))))
 (defun show-tables (line stop day)
-  (show-tables-raw (format nil "rozklady.mpk.krakow.pl/aktualne/~4,'0D/~4,'0Dt~3,'0D.htm.html" line line stop)
-		   day))
+  (show-tables-raw (mpk-get-filename line stop) day))
